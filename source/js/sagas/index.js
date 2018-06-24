3fe649adaa10c9a4dda7f31a6a10df9fee83e34e
Hexa Blue <hexa6lue@gmail.com>
2018-06-24 14:15:41 +0200

    include sagas: triz

--- source/js/sagas/index.js
+++ source/js/sagas/index.js
@@ -1,9 +1,11 @@
 import { all } from 'redux-saga/effects';
 
 import peopleSagas from 'sagas/people';
+import trizSagas from 'sagas/triz';
 
 export default function* rootSaga() {
   yield all([
     ...peopleSagas,
+    ...trizSagas,
   ]);
 }
