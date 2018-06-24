bb657cf4799f51cfcc861cb2ab98dab89da8f838
Hexa Blue <hexa6lue@gmail.com>
2018-06-24 14:12:25 +0200

    include reducer: triz

--- source/js/reducers/index.js
+++ source/js/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import app from 'reducers/app';
 import people from 'reducers/people';
+import triz from 'reducers/triz/triz';
 
 export default combineReducers({
   app,
   people,
+  triz,
 });
