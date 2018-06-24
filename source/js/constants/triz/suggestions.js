150517ec4e05d14817adba0481cbb9c3724333d4
Hexa Blue <hexa6lue@gmail.com>
2018-06-24 14:06:23 +0200

    add triz constants

---
+++ source/js/constants/triz/suggestions.js
@@ -0,0 +1,172 @@
+/* eslint-disable max-len */
+export default [
+  [
+    'Divide an object into independent parts.',
+    'Make an object sectional.',
+    "Increase the degree of an object's segmentation.",
+  ],
+  [
+    'Extract (remove or separate) a "disturbing" part or property from an object.',
+    'Extract only the necessary part or property.',
+  ],
+  [
+    'Provide transition from a homogenous structure of an object or outside environment (outside action) to a heterogeneous structure.',
+    'Have different parts of an object carry out different functions.',
+    'Place each part of the object under conditions most favourable for its operation.',
+  ],
+  [
+    'Replace a symmetrical form with an asymmetrical form of the object.',
+    'If an object is already asymmetrical, increase the degree of asymmetry.',
+  ],
+  [
+    'Combine in space homogeneous objects or objects destined for contiguous operations.',
+    'Combine in time homogeneous or contiguous operations.',
+  ],
+  [
+    'Have the object perform multiple functions, thereby eliminating the need for other objects.',
+  ],
+  [
+    'Contain an object inside another, which in turn is placed inside another object.',
+    'An object passes through a cavity in another object.',
+  ],
+  [
+    "Compensate for the object's weight by joining with another object that has lifting force.",
+    "Compensate for the object's weight by providing aerodynamic or hydrodynamic forces.",
+  ],
+  [
+    'If it is necessary to carry out some action, consider a counteraction in advance.',
+    'If an object must be in tension, provide anti-tension in advance.',
+  ],
+  [
+    'Carry out the required action in advance, in full or in part.',
+    'Arrange objects so that they can go into action, without time loss while waiting for the action (and from the most convenient position)',
+  ],
+  [
+    'Compensate for the relatively low reliability of an object by countermeasures taken in advance.',
+  ],
+  [
+    'Change the condition of work so that an object need not be raised or lowered.',
+  ],
+  [
+    'Instead of an action dictated by the specifications of the problem, implement an opposite action.',
+    'Make object a moving part, or make non-moving part moveable and outside environment immovable.',
+    'Turn object upside down.',
+  ],
+  [
+    'Replace linear parts or flat surfaces with curved ones, and cubical shapes with spherical shapes.',
+    'Use rollers, balls and spirals.',
+    'Replace a linear motion with a rotating motion, utilise a centrifugal force.',
+  ],
+  [
+    'Make characteristics of an object or outside environment automatically adjust for optimal performance at each stage of the operation.',
+    'Divide an object into elements able to change position relative to each other.',
+    'If an object is immovable, make it moveable or interchangeable.',
+  ],
+  [
+    'If it is difficult to obtain 100% of a desired effect, achieve somewhat more or less to greatly simplify the problem.',
+  ],
+  [
+    'Remove problems of moving an object in a line by allowing two-dimensional movement along a plane.',
+    'Similarly, problems in moving an object in a plane are removed if the object can be changed to allow three-dimensional movement.',
+    'Use multi-layer assembly of objects instead of a single layer.',
+    'Incline the object or turn it "on its side".',
+    'Project images on to neighbouring areas or on to the reverse side of the object.',
+  ],
+  [
+    'Set an object into oscillation.',
+    'If an oscillation exists, increase its frequency, even as far as ultrasonic.',
+    'Use the frequency of resonance.',
+    'Instead of mechanical vibrators, use piezovibrators.',
+    'Use ultrasonic vibrations in conjunction with an electronic field',
+  ],
+  [
+    'Replace a continuous action with a periodic one (impulse).',
+    'If an action is already periodic, change its frequency.',
+    'Use pulses between impulses to provide additional action.',
+  ],
+  [
+    'Carry out an action without a break - all parts of an object should be constantly operating at full capacity.',
+    'Remove an idle and intermediate motion.',
+  ],
+  [
+    'Perform harmful or hazardous operations at very high speed.',
+  ],
+  [
+    'Utilise a harmful factor or harmful effect of an environment to obtain a positive effect.',
+    'Remove a harmful factor by combining it with another harmful factor.',
+    'Increase the amount of a harmful action until it ceases to be harmful.',
+  ],
+  [
+    'Introduce feedback.',
+    'If feedback already exists, reverse it.',
+  ],
+  [
+    'Use an intermediary object to transfer or carry out an action.',
+    'Temporarily connect an object to another one that is easy to remove.',
+  ],
+  [
+    'Make the object service itself and carry out supplementary and repair operations.',
+    'Make use of waste material and energy',
+  ],
+  [
+    'Use a simple or inexpensive copy instead of an object which is complex, expensive, fragile or inconvenient to use.',
+    'Replace an object or system of objects by an optical copy or image. A scale can be used to reduce or enlarge the object.',
+    'If visible or optical copies are used, replace them with infrared or ultraviolet copies.',
+  ],
+  [
+    'Replace an expensive object by a collection of inexpensive ones, compromising other properties (longevity, for example).',
+  ],
+  [
+    'Replace a mechanical system by an optical, acoustical or odour system.',
+    'Use an electrical, magnetic or electromagnetic field for interaction with the object.',
+    'Replace fields.',
+    'Use a field in conjunction with ferromagnetic particles.',
+  ],
+  [
+    'Replace solid parts of an object with gas or liquid. These parts can use air or water for inflation or use air or hydrostatic cushions.',
+  ],
+  [
+    'Replace customary construction with flexible membrane and thin film.',
+    'Isolate an object from the outside environment with a thin film or fine membrane.',
+  ],
+  [
+    'Make an object porous or use additional porous elements (inserts, covers, etc.).',
+    'If an object is already porous, fill the pores in advance with some substance.',
+  ],
+  [
+    'Change the colour of an object or its surroundings.',
+    'Change the translucency of an object or its surroundings.',
+    'Use colour additives to observe difficult to see objects or processes.',
+    'If such additives are already used, employ luminescent traces or tracer elements.',
+  ],
+  [
+    'Make objects interact with a primary object of the same material or a material similar in behaviour.',
+  ],
+  [
+    'After it has completed its function or become useless, reject or modify (e.g. discard, dissolve or evaporate) an element of an object.',
+    'Restore directly any used-up part of an object.',
+  ],
+  [
+    'Change the aggregate state of an object, the concentration of density, the degree of flexibility or the temperature.',
+  ],
+  [
+    'Implement an effect developed during the phase transition of a substance. For instance, during liberation of absorption of heat.',
+  ],
+  [
+    'Use expansion or contraction of a material by heat.',
+    'Use various materials with different coefficients of heat expansion.',
+  ],
+  [
+    'Replace normal air with enriched air.',
+    'Replace enriched air with oxygen.',
+    'In oxygen or in air, treat a material with ionising radiation.',
+    'Use ionised oxygen.',
+  ],
+  [
+    'Replace the normal environment with an inert one.',
+    'Carry out process in a vacuum.',
+  ],
+  [
+    'Replace a homogeneous material with a composite one.',
+  ],
+];
